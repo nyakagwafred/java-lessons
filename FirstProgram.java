0e2b26693d1f39d8345f6d72d23aa1619ad9c2ec
Fred O Nyakagwa <fonyakagwa@usiu.ac.ke>
2024-01-12 21:34:31 +0300

    Initial commit

---
+++ FirstProgram.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello out there"); 
+	System.out.println("I will add two numbers for you"); 
+	System.out.println("Enter two whole numbers in a line"); 
+
+	int n1, n2;
+
+	Scanner keyboard = new Scanner(System.in);
+	n1 = keyboard.nextInt();
+	n2 = keyboard.nextInt();
+
+	System.out.println("The sum of the two numbers is..."); 
+	System.out.println(n1 + n2); 
+	
+    }
+}
---
+++ Hello2.java
@@ -0,0 +1,6 @@
+public class Hello2 {
+    public void main() {
+        System.out.println("Hello there!!"); 
+	
+    }
+}
